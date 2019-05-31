95f3ea0847d1e49dab2171936dd7add597552437
yyllyy <yyllyy0813@163.com>
2019-05-31 16:37:50 +0800

    texi

--- main/main.js
+++ main/main.js
@@ -1,4 +1,17 @@
 module.exports = function main(inputs) {
-    // write your code here...
-    return "price";
+    var dis = inputs["distance"];
+    var time = inputs["parkTime"];
+    var res = 0;
+    if(dis<=2){
+        res = 6+0.25*time;
+    }
+    else if(dis>2&&dis<=8){
+        res = 0.8*(dis-2)+6+0.25*time;
+    }
+    else if(dis>8){
+        res = 10.8+(dis-8)*1.2+0.25*time;
+    }
+
+    res = Math.round(res);
+    return res;
 };
